3a914468a60ddf006255b7471fad2109dffb6f1e
Ynon Perek <ynonperek@gmail.com>
2013-12-06 11:02:58 +0200

    Modified Members.pm to automatically chain on login required

--- templates/default/features/UserManagement/Members.pm
+++ templates/default/features/UserManagement/Members.pm
@@ -4,9 +4,21 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller' }
 
-sub m : Chained('/login/required') PathPart('members') CaptureArgs(0) {}
+__PACKAGE__->config(
+  action => {
+    '*' => { Chained => 'base' },
+    base => { Chained => '/login/required', PathPart => 'members', CaptureArgs => 0 },
+  },
+);
 
-sub hello :Chained(m) {
+sub base {}
+
+# All actions in this controller are automatically chained
+# on /login/required
+# Note: don't use Local or Path when defining new actions,
+# this will break the chain. Simply use GET, POST, etc.
+
+sub hello :GET {
   # An action for /members/hello
   # Only accessible for logged-in users
   my ( $self, $c ) = @_;
